c2cabc6811f75b5c67785fb898737f10722f044f
Pedro Henriques <pedro@pedrojhenriques.com>
2025-12-11 17:59:57 +0000

    Update script that bumps dependency versions.

--- cli/dependencies_update.sh
+++ cli/dependencies_update.sh
@@ -17,22 +17,29 @@ while [ "$#" -gt 0 ]; do
   esac
 done
 
-OPTS="";
+CMD="mvn -B versions:display-dependency-updates -DprocessAllModules=true -DprocessParent=true -DprocessPlugins=true";
 if [ $UPDATE -eq 1 ]; then
-  OPTS="${OPTS} -u";
   if [ $UPDATE_PROMPT -eq 1 ]; then
-    OPTS="${OPTS}:Prompt";
+    echo "This will update dependency/plugin/parent versions in your pom.xml files";
+    echo "using the Maven Versions Plugin (use-latest-releases).";
+    echo "No backup POMs will be created. Continue? [y/N]";
+    read ans;
+    case "$ans" in
+      y|Y|yes|YES) ;;
+      *) echo "Aborted."; exit 1;;
+    esac
   fi
+
+  CMD="mvn -B versions:use-latest-releases -DprocessAllModules=true -DprocessParent=true -DprocessPlugins=true -DgenerateBackupPoms=false";
 fi
 
-CMD="dotnet tool restore; dotnet outdated ${OPTS} ./*.sln";
 if [ $USE_DOCKER -eq 1 ]; then
   INTERACTIVE_FLAGS="-it";
   if [ $RUNNING_IN_PIPELINE -eq 1 ]; then
     INTERACTIVE_FLAGS="-i";
   fi
 
-  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" mcr.microsoft.com/dotnet/sdk:8.0-noble /bin/sh -c "${CMD}";
+  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" maven:3.9-eclipse-temurin-21 /bin/sh -c "${CMD}";
 else
   eval "${CMD}";
 fi
